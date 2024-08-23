c52dc6889e81eef48d1ee026b1bf5474e106dd6a
karinelisboa <karinelisboarodrigues@gmail.com>
2024-08-23 20:54:38 -0300

    Login e esqueci a senha feitos

---
+++ sw-gestao-ecommerce/src/app/(pages)/home/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div
+      className="flex items-center justify-center min-h-screen"
+      style={{
+        fontFamily: 'Inter, sans-serif',
+        color: 'black',
+        fontSize: '24px',
+        fontWeight: 600,
+        textAlign: 'center',
+      }}
+    >
+      <p>Ainda construindo o home principal! Pode ir saindo.</p>
+    </div>
+  );
+};
+
+export default HomePage;
