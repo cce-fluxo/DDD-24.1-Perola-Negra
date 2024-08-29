1583ba496ac89d781bc950c29a140ddd8b9006c3
Carolina Santiago <ctt.carolinasantiago@gmail.com>
2024-08-29 19:04:03 -0300

    header responsiva

--- sw-gestao-ecommerce/src/app/(pages)/cupons/page.tsx
+++ sw-gestao-ecommerce/src/app/(pages)/cupons/page.tsx
@@ -3,11 +3,9 @@ import Header from "@/app/components/Header";
 import CardMobile from "@/app/components/tabela/CardMobile";
 import NavegacaoCupons from "@/app/components/tabela/NavegacaoCupons";
 import Tabela from "@/app/components/tabela/Tabela";
-import HeaderMobile from "@/app/components/HeaderMobile/HeaderMobile";
 
 function Cupons() {
   {
-
   }
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center bg-[#F2F2F2]">
@@ -15,19 +13,20 @@ function Cupons() {
 
       <div className="flex flex-col justify-around h-[100vh] w-[80vw] py-3 lg:w-[99vw]">
         {/*Pagina*/}
-        
-        <HeaderMobile titulo="Cupons"></HeaderMobile>{/**Header de dispositivos mobile + md */}
-        <div className="hidden lg:flex"><Header></Header></div>
 
-        <div className="hidden w-[40vw] h-[7vh] lg:flex lg:ml-3">{/*NavegacaoCupom LG --> Só ativa para telas grandes*/}
+        <Header titulo="Cupons"></Header>
+        {/**Header de dispositivos mobile + md */}
+
+        <div className="hidden w-[40vw] h-[7vh] lg:flex lg:ml-3">
+          {/*NavegacaoCupom LG --> Só ativa para telas grandes*/}
           <NavegacaoCupons activeLink={1}></NavegacaoCupons>
         </div>
 
-        <div className="w-full h-[75vh] overflow-y-auto lg:w-[85vw] lg:h-[65vh] lg:mx-auto lg:rounded-t-2xl">{/**rounded-t-2xl impede que as celulas da tabela vazem pela head ao scrollar*/}
+        <div className="w-full h-[75vh] overflow-y-auto lg:w-[85vw] lg:h-[65vh] lg:mx-auto lg:rounded-t-2xl">
+          {/**rounded-t-2xl impede que as celulas da tabela vazem pela head ao scrollar*/}
           {/* Conteudo principal*/}
 
           <div className="flex flex-col md:flex-row md:flex-wrap md:justify-evenly lg:hidden">
-
             {/* Conteúdo mobile até md */}
             <CardMobile
               nome="CUPOM10"
@@ -49,7 +48,8 @@ function Cupons() {
           </div>
         </div>
 
-        <div className="w-full h-[8vh] lg:hidden">{/**Navegacao para dispositivos mobile*/}
+        <div className="w-full h-[8vh] lg:hidden">
+          {/**Navegacao para dispositivos mobile*/}
           <NavegacaoCupons activeLink={1}></NavegacaoCupons>
         </div>
       </div>
---
+++ sw-gestao-ecommerce/src/app/components/HeaderLarge.tsx
@@ -0,0 +1,71 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import leftChevron from "../../../public/icons/left-chevron.png";
+import { usePathname } from "next/navigation";
+
+interface HeaderProps {
+  titulo?: string; // Título opcional, "LOGO" será o default
+}
+
+const HeaderLarge: React.FC<HeaderProps> = ({ titulo = "LOGO" }) => {
+  const router = useRouter();
+  const currentPath = usePathname();
+
+  // Função para verificar se a URL atual contém a string especificada
+  const isActive = (path: string) => currentPath.startsWith(path);
+
+  // Função para determinar o texto e o link a partir do pathname
+  const getLinkText = (path: string) => {
+    switch (path) {
+      case "/produtos":
+        return "Produtos";
+      case "/banner":
+        return "Banner";
+      case "/fluxo-pedidos":
+        return "Fluxo de Pedidos";
+      case "/controle-estoque":
+        return "Controle de Estoque";
+      case "/cupons":
+        return "Cupons";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <div className="bg-transparent w-full py-4 px-4 sm:px-8 lg:px-10 flex flex-col sm:flex-row items-start justify-between fixed top-0 z-50">
+      <div className="flex items-center gap-4 sm:pt-8 sm:gap-6">
+        <Image
+          src={leftChevron}
+          alt="Voltar"
+          className="object-none cursor-pointer"
+          onClick={() => router.push("/home")}
+        />
+        <Link href={"/home"}>
+          <h1 className="font-bold text-3xl sm:text-5xl">{titulo}</h1>
+        </Link>
+      </div>
+      <ul className="flex flex-col sm:flex-row gap-4 sm:gap-8 font-semibold">
+        {[
+          "/banner",
+          "/produtos",
+          "/fluxo-pedidos",
+          "/controle-estoque",
+          "/cupons",
+        ].map((path) => (
+          <li
+            key={path}
+            className={isActive(path) ? "underline underline-offset-2" : ""}
+          >
+            <Link href={path}>{getLinkText(path)}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default HeaderLarge;
