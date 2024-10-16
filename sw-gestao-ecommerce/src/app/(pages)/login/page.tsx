bb2122c2ffdad970a2db6052d83b93f3950cf795
Roberto-Ceschini <robert17ceschini@gmail.com>
2024-10-16 20:39:47 -0300

    Update tela login

--- sw-gestao-ecommerce/src/app/(pages)/login/page.tsx
+++ sw-gestao-ecommerce/src/app/(pages)/login/page.tsx
@@ -1,95 +1,57 @@
-"use client"
-import React from "react";
-import Image from "next/image";
-import Link from "next/link"; // Importando Link do Next.js
-import { ErrorMessage, Field, Formik, Form } from "formik";
-import * as Yup from 'yup';
-import { useRouter } from "next/navigation";
+"use client";
 
-const LoginPage: React.FC = () => {
-  const rota = useRouter(); //usado para criar a rota do botao submit
+import React from "react";
+import { useRouter } from "next/navigation";
+import LoginLayout from "@/app/components/LoginLayout"; // Importando o componente do background
+
+const LoginForm: React.FC = () => {
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    router.push("/home"); // Ir para outra página
+  };
 
   return (
-    <div className="flex h-screen bg-white">
-      <div className="flex w-full md:w-3/4 bg-white relative z-10">
-        <div className="flex flex-col items-start justify-between bg-gray-100 rounded-lg w-1/2 h-3/4 mx-16 mt-24 p-8">
-          <div className="flex flex-col mt-8 ml-8">
-            <h1 className="text-5xl font-bold">LOGO</h1>
-            <h2 className="text-5xl font-bold mt-4">RESTAURANTE</h2>
-          </div>
+    // Layout do formulário de login
+    <LoginLayout>
+      <div
+        className="absolute w-[300px] h-[60px] left-[85px] top-[280px] text-black text-[52px] font-semibold break-words"
+      >
+        Entrar
+      </div>
 
-          <div className="flex flex-col items-start mt-8 ml-8">
-            <h3 className="text-6xl font-bold mb-4">Entrar</h3>
-
-            <Formik
-              initialValues={{email: '', senha: ''}}
-              validationSchema={Yup.object({
-                email: Yup.string().email('Email inválido').required('Bota o email, mano!'),
-                senha: Yup.string().min(6, 'a senha deve ter no minimo 6 caracteres').required('bota a senha, mano!'),
-              })}
-              onSubmit={(values, { setSubmitting }) => {
-                console.log(values);
-                rota.push('/home');
-                setSubmitting(false); // Libera o estado de envio
-              }}
-            >
-              <Form className="flex flex-col mt-8">
-                <label className="text-lg font-semibold" htmlFor="email">E-mail</label>
-                <Field
-                  id = "email"
-                  name="email"
-                  type= "text"
-                  className="mt-4 p-2 border border-gray-300 rounded w-full"
-                  style={{ minWidth: '450px' }}
-                  placeholder="Digite seu e-mail"
-                />
-                <ErrorMessage name = "email" component="div" className="text-red-500 text-sm"/>
-
-
-                <label className="text-lg font-semibold mt-4" htmlFor="senha">Senha</label>
-                <Field
-                  id = "senha"
-                  name = "senha"
-                  type = "password"
-                  className="mt-2 p-2 border border-gray-300 rounded w-full"
-                  placeholder="Digite sua senha"
-                />
-                <ErrorMessage name="senha" component="div" className="text-red-500 text-sm"/>
-                {/* Link "Esqueci minha senha" logo abaixo do input da senha */}
-                <Link href="/recuperar-senha" className="text-gray-500 text-sm mt-1 self-end hover:underline">
-                  Esqueci minha senha
-                </Link>
-                <button type="submit" className=" border-2">
-                  Submit</button>
-              </Form>
-            </Formik>
-
-          </div>
-
-          <div className="flex mt-8 ml-48">
-            {/* Botão "Entrar" agora redireciona para a página desejada */}
-            <Link href="/home" className="bg-black text-white rounded-full py-4 px-14 text-center">
-              Entrar
-            </Link>
-          </div>
+      <form onSubmit={handleSubmit} className="absolute flex flex-col items-center justify-center p-8 left-[55px] top-[360px] w-[calc(100%-70px)] max-w-[500px]">
+        <div className="mb-4 w-full">
+          <label className="block text-sm font-bold text-gray-700 mb-2" htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg"
+            placeholder="Digite seu email"
+          />
         </div>
-      </div>
-
-      <div className="flex flex-col justify-center items-center w-1/2 bg-gray-100 relative z-0">
-        <div className="absolute top-0 left-0 w-full h-full bg-gray-300"></div>
-      </div>
-
-      <div className="absolute z-50" style={{ top: '34%', left: '39%'}}>
-        <Image
-          src="/images/mobilegame.png"
-          alt="Mobile Game"
-          width={700}
-          height={400}
-        />
-      </div>
-    </div>
+        <div className="mb-4 w-full">
+          <label className="block text-sm font-bold text-gray-700 mb-2" htmlFor="password">Senha</label>
+          <input
+            type="password"
+            id="password"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg"
+            placeholder="Digite sua senha"
+          />
+          <a href="/login/esqueci-minha-senha-1" className="block text-sm text-gray-500 mt-2 hover:underline text-right">
+            Esqueci minha senha
+          </a>
+        </div>
+        <button
+          type="submit"
+          className="absolute left-[200px] top-[270px] w-[150px] px-12 py-3.5 text-lg font-bold text-center whitespace-nowrap bg-black rounded-3xl text-neutral-100"
+        >
+          Entrar
+        </button>
+      </form>
+    </LoginLayout>
   );
 };
 
-export default LoginPage;
-
+export default LoginForm;
