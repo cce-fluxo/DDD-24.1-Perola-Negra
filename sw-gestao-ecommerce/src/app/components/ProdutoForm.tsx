1d478ac977706a96c5fd07045f751d65aa55642a
Breno Motta <brenormotta.20231@poli.ufrj.br>
2024-11-10 13:21:54 -0300

    Update ProdutoForm.tsx

--- sw-gestao-ecommerce/src/app/components/ProdutoForm.tsx
+++ sw-gestao-ecommerce/src/app/components/ProdutoForm.tsx
@@ -1,13 +1,26 @@
 "use client";
+
+
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import PopupConfirm from "./PopupProduto"; // Importe o PopupConfirm aqui
+import { Field, Formik, Form, ErrorMessage, useFormik, useFormikContext } from "formik";
+import * as Yup from 'yup';
+
+
+const formatCurrency = (value: string) => {
+  const numericValue = parseFloat(value.replace(/[^\d]/g, '')) / 100;
+  return numericValue.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+};
+
 
 const ProdutoForm: React.FC = () => {
   const [quantidade, setQuantidade] = useState("");
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [popupVisible, setPopupVisible] = useState(false); // Estado para o Popup
+
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -20,10 +33,16 @@ const ProdutoForm: React.FC = () => {
     }
   };
 
+
   const handleSelect = (value: string) => {
     setQuantidade(value);
     setDropdownOpen(false);
   };
+
+
+  const router = useRouter();
+  const isSubmiting = useFormikContext();
+
 
   return (
     <div className="mb-4 bg-neutral-100 px-5 py-12 rounded-3xl w-full flex flex-col md:flex-row justify-between md:justify-evenly gap-5 md:gap-[5%]">
@@ -61,69 +80,88 @@ const ProdutoForm: React.FC = () => {
         />
       </div>
 
-      <div className="flex flex-col justify-between w-full md:w-1/4 text-neutral-600">
-        <input
-          className="text-lg font-medium placeholder:text-neutral-600 underline underline-offset-4 decoration-zinc-200 bg-transparent focus:outline-none"
-          placeholder="Nome do produto"
-        />
-        <textarea
-          className="underline underline-offset-4 decoration-zinc-200 py-4 bg-transparent placeholder:text-neutral-600 focus:outline-none resize-none"
-          placeholder="Faça uma breve descrição do produto e dos elementos que ele contém"
-          rows={3}
-        />
-        <div className="flex items-center font-medium">
-          <span className="mr-1">R$</span>
-          <input
-            className="bg-transparent placeholder:text-neutral-600 focus:outline-none"
-            placeholder="0,00"
-            type="currency"
-          />
-        </div>
-        <div className="relative bg-neutral-200 w-11/12 rounded-xl py-1.5 px-3 mt-3 flex justify-between items-center">
-          <button
-            onClick={() => setDropdownOpen(!dropdownOpen)}
-            className="flex items-center justify-between w-full"
-          >
-            <span className="mr-2 text-left">
-              {quantidade || "Insira a quantidade total do produto"}
-            </span>
-            <Image
-              className="self-center"
-              src={"/icons/down-chevron.png"}
-              alt="Seta para baixo"
-              width={15}
-              height={20}
-            />
-          </button>
-          {dropdownOpen && (
-            <ul className="absolute top-full left-0 mt-1 w-full bg-white border rounded-lg shadow-lg">
-              {["abaixo de 1000", "1000", "2000", "3000", "4000", "5000+"].map(
-                (value) => (
-                  <li
-                    key={value}
-                    onClick={() => handleSelect(value)}
-                    className="px-4 py-2 cursor-pointer hover:bg-neutral-100"
-                  >
-                    {value}
-                  </li>
-                )
-              )}
-            </ul>
-          )}
-        </div>
-      </div>
 
-      <div className="flex flex-row gap-5 mt-6 md:mt-0 place-self-end">
-        <button
-          onClick={() => setPopupVisible(true)} // Abre o popup ao clicar em "Excluir"
-          className="py-1 px-6 bg-neutral-500 text-white rounded-xl"
-        >
-          Excluir
-        </button>
-        <button className="py-1 px-6 bg-neutral-500 text-white rounded-xl">
-          Salvar
-        </button>
-      </div>
+      <Formik
+        initialValues={{
+          nome: '',
+          descricao: '',
+          valor: '',
+          quantidade: '',
+        }}
+        validationSchema={Yup.object({
+          nome: Yup.string().required('Insira o nome do produto'),
+          descricao: Yup.string().required('Insira a descrição do produto'),
+          valor: Yup.string().required('Insira o valor do produto'),
+          quantidade: Yup.number().typeError('Quantidade deve ser um número').required('Insira a quantidade do produto').min(1, 'Quantidade mínima é 1'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log(values);
+          console.log(isSubmiting);
+          router.push('/produtos/categoria-1');
+          setSubmitting(false);
+        }}
+      >
+        {({ values, setFieldValue, isSubmitting }) => (
+          <Form className="flex flex-col md:flex-row gap-4">
+            <div className="flex flex-col justify-between w-full mmmmd:w-1/4 text-neutral-600">
+              <Field
+                name="nome"
+                type="text"
+                className="text-lg font-medium placeholder:text-neutral-600 underline underline-offset-4 decoration-zinc-200 bg-transparent focus:outline-none"
+                placeholder="Nome do produto"
+              />
+              <ErrorMessage name="nome" component="div" className="text-red-500" />
+
+
+              <Field
+                name="descricao"
+                as="textarea"
+                className="underline underline-offset-4 decoration-zinc-200 py-4 bg-transparent placeholder:text-neutral-600 focus:outline-none resize-none"
+                placeholder="Faça uma breve descrição do produto e dos elementos que ele contém"
+                rows={3}
+              />
+              <ErrorMessage name="descricao" component="div" className="text-red-500" />
+
+
+              <Field
+                name="valor"
+                type="text"
+                value={values.valor}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setFieldValue("valor", formatCurrency(e.target.value))
+                }
+                className="flex items-center font-medium bg-transparent placeholder:text-neutral-600 focus:outline-none"
+                placeholder="R$ 0,00"
+              />
+              <ErrorMessage name="valor" component="div" className="text-red-500" />
+
+
+              <Field
+                name="quantidade"
+                type="number"
+                className="flex items-center font-medium bg-neutral-200 placeholder:text-neutral-600 focus:outline-none"
+                placeholder="Quantidade"
+              />
+              <ErrorMessage name="quantidade" component="div" className="text-red-500" />
+            </div>
+
+
+            <div className="flex flex-row gap-5 mt-6 md:mt-0 place-self-end">
+              <button
+                type="button"
+                onClick={() => setPopupVisible(true)}
+                className="py-1 px-6 bg-neutral-500 text-white rounded-xl"
+              >
+                Excluir
+              </button>
+              <button type="submit" className="py-1 px-6 bg-neutral-500 text-white rounded-xl">
+                Salvar
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+
 
       {popupVisible && (
         <PopupConfirm
@@ -138,5 +176,5 @@ const ProdutoForm: React.FC = () => {
   );
 };
 
-export default ProdutoForm;
 
+export default ProdutoForm;
